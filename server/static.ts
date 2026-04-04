fa18542cd3bb0eb3057e003e6105e800e2036679
sanchamit lepcha <sanchamitl088@gmail.com>
2026-04-04 18:15:58 +0530

    static file changed

--- server/static.ts
+++ server/static.ts
@@ -1,9 +1,17 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// This replaces the missing __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  // We go up one level (..) because this file is in 'server/' 
+  // but the 'public' folder is inside 'dist/' or the root
+  const distPath = path.resolve(__dirname, "..", "public");
+  
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -13,7 +21,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  app.get("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
